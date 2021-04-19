9e49bd8237c5a2c474aed6ca2b8dd4a9aed90749
Alec Scott <alec@bcs.sh>
2021-04-19 12:34:57 -0700

    Version Bump

--- config/config.go
+++ config/config.go
@@ -55,7 +55,7 @@ func init() {
 }
 
 func defaultConfig() {
-	Global.General.Version = "0.2.1"
+	Global.General.Version = "0.2.2"
 	Global.Parsers.Loaded = "spack,shpc"
 	Global.Branch.Prefix = "binoc/"
 	Global.PR.IgnoreLabel = "binoc-blacklist"
