3840d8a031ed9c9787ef3e2394b98c7d2f466ce3
Alec Scott <alec@bcs.sh>
2021-04-22 16:04:23 -0700

    Update Binoc to v0.2.6

--- config/config.go
+++ config/config.go
@@ -55,7 +55,7 @@ func init() {
 }
 
 func defaultConfig() {
-	Global.General.Version = "0.2.5"
+	Global.General.Version = "0.2.6"
 	Global.Parsers.Loaded = "spack,shpc"
 	Global.Branch.Prefix = "binoc/"
 	Global.PR.IgnoreLabel = "binoc-blacklist"
