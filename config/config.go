37850672e4af870d2a225fe280bf028f5bf73a68
Alec Scott <alec@bcs.sh>
2021-05-21 16:21:05 -0700

    Fix Version

--- config/config.go
+++ config/config.go
@@ -56,7 +56,7 @@ func init() {
 }
 
 func defaultConfig() {
-	Global.General.Version = "0.2.7"
+	Global.General.Version = "0.2.8"
 	Global.Parsers.Loaded = "spack,shpc"
 	Global.Branch.Prefix = "binoc/"
 	Global.PR.IgnoreLabel = "binoc-blacklist"
